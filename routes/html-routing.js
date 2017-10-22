bd1d9f884124363b0e7fe6940823de4223f132f8
Alfredo Rodriguez <alrod909@gmail.com>
2017-10-22 02:06:12 -0600

    Fixes to CSS

--- routes/html-routing.js
+++ routes/html-routing.js
@@ -3,8 +3,7 @@
 // Date: 10/20/2017
 
 
-
-module.exports = function getSites(app){
+module.exports = function getSites(app) {
 
     // Basic route that sends the user first to the AJAX Page
     //Main Page Route
@@ -15,19 +14,15 @@ module.exports = function getSites(app){
                 title: 'Mongo Scrapper || Home', css: '/assets/css/scraper_style.css',
                 icon: "http://res.cloudinary.com/alrod909/image/upload/v1508485964/mongo-scrapper/icon.png",
 
-                stylesheet: ['https://fonts.googleapis.com/css?family=Ubuntu',
-                    'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css',
-                    'https://fonts.googleapis.com/icon?family=Material+Icons',
-                    'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css']
-
             });
     });
 
     app.get('/articles-saved', function (req, res) {
-        res.render('articlesSaved', { title: 'Mongo Scrapper || Articles Saved', css: '/assets/css/scraper_style.css',
-            icon: "http://res.cloudinary.com/alrod909/image/upload/v1508485964/mongo-scrapper/icon2.png"});
+        res.render('articles-saved', {
+            title: 'Mongo Scrapper || Articles Saved', css: '/assets/css/scraper_style.css',
+            icon: "http://res.cloudinary.com/alrod909/image/upload/v1508485964/mongo-scrapper/icon2.png"
+        });
     });
-
 
 
 };
