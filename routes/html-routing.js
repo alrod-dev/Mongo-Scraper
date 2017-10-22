2bfbe2109e9e1a91f4e7d4a0ba8e901fd32b9d64
Alfredo Rodriguez <alrod909@gmail.com>
2017-10-22 17:55:43 -0600

    Some Fixes CSS

--- routes/html-routing.js
+++ routes/html-routing.js
@@ -2,7 +2,6 @@
 // File: JS - html-routing.js
 // Date: 10/20/2017
 
-
 module.exports = function getSites(app) {
 
     // Basic route that sends the user first to the AJAX Page
@@ -11,15 +10,22 @@ module.exports = function getSites(app) {
         res.render('index',
 
             {
-                title: 'Mongo Scrapper || Home', css: '/assets/css/scraper_style.css',
-                icon: "http://res.cloudinary.com/alrod909/image/upload/v1508485964/mongo-scrapper/icon.png",
+                title: 'Mongo Scrapper | Home', css: '/assets/css/scraper_style.css',
+                icon: "http://res.cloudinary.com/alrod909/image/upload/v1508485964/mongo-scrapper/icon.png"
 
             });
     });
 
+    app.get('/scrape', function (req, res) {
+        res.render('scrape-articles', {
+            title: 'Mongo Scrapper | Scrape', css: '/assets/css/scraper_style.css',
+            icon: "http://res.cloudinary.com/alrod909/image/upload/v1508714412/mongo-scrapper/scraper.png"
+        });
+    });
+
     app.get('/articles-saved', function (req, res) {
         res.render('articles-saved', {
-            title: 'Mongo Scrapper || Articles Saved', css: '/assets/css/scraper_style.css',
+            title: 'Mongo Scrapper | Articles Saved', css: '/assets/css/scraper_style.css',
             icon: "http://res.cloudinary.com/alrod909/image/upload/v1508485964/mongo-scrapper/icon2.png"
         });
     });
